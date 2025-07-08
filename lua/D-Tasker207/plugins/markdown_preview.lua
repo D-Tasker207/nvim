bf50211e9dc379763a566d8a2c58b74c8c2f4f35
Duncan Tasker <tasatree@gmail.com>
2025-07-08 17:40:06 -0400

    added markdown_preview plugin

---
+++ lua/D-Tasker207/plugins/markdown_preview.lua
@@ -0,0 +1,12 @@
+-- Markdown Preview Plugin
+
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function() 
+    vim.g.mkdp_auto_start = 1
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
